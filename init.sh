41b398ddf6d4571d34b5cd14f40e920b77fa7efe
Eric D. Schabell <eric@schabell.org>
2015-11-29 11:21:48 -0500

    Error chking added for patching.

--- init.sh
+++ init.sh
@@ -94,6 +94,12 @@ echo "Applying JBoss EAP 6.4.4 patch now..."
 echo
 $JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH"
 
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during JBoss EAP patching!
+	exit
+fi
+
 echo
 echo "JBoss BPM Suite installer running now..."
 echo
