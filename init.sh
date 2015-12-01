3bef4cb0edd1bc7c9a73cbc0dc8115061c451e06
Eric D. Schabell <eric@schabell.org>
2015-12-01 18:43:51 -0500

    Adjusted formatting output.

--- init.sh
+++ init.sh
@@ -111,6 +111,7 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
+echo
 echo "  - enabling demo accounts role setup in application-roles.properties file..."
 echo
 cp $SUPPORT_DIR/application-roles.properties $SERVER_CONF
