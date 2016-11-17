faa9e01c40451f00bbed98a807395e90c1097baf
Duncan Doyle <ddoyle@redhat.com>
2016-11-17 11:16:52 +0100

    Upgraded Linux/macOS installer script to JBoss EAP 7.0 and JBoss BPM Suite 6.4.0.GA. Also introduced new demo user: 'bpmsAdmin'.

--- init.sh
+++ init.sh
@@ -1,71 +1,72 @@
-#!/bin/sh 
+#!/bin/sh
 DEMO="Install Demo"
 AUTHORS="Andrew Block, Eric D. Schabell"
 PROJECT="git@github.com:jbossdemocentral/bpms-install-demo.git"
 PRODUCT="JBoss BPM Suite"
-JBOSS_HOME=./target/jboss-eap-6.4
+TARGET=./target
+JBOSS_HOME=$TARGET/jboss-eap-7.0
 SERVER_DIR=$JBOSS_HOME/standalone/deployments
 SERVER_CONF=$JBOSS_HOME/standalone/configuration/
 SERVER_BIN=$JBOSS_HOME/bin
 SRC_DIR=./installs
 SUPPORT_DIR=./support
 PRJ_DIR=./projects
-BPMS=jboss-bpmsuite-installer-6.2.0.BZ-1299002.jar
-EAP=jboss-eap-6.4.0-installer.jar
-EAP_PATCH=jboss-eap-6.4.4-patch.zip
-VERSION=6.2
+BPMS=jboss-bpmsuite-6.4.0.GA-deployable-eap7.x.zip
+EAP=jboss-eap-7.0.0-installer.jar
+#EAP_PATCH=jboss-eap-6.4.4-patch.zip
+VERSION=6.4
 
 # wipe screen.
-clear 
+clear
 
 echo
 echo "#################################################################"
-echo "##                                                             ##"   
+echo "##                                                             ##"
 echo "##  Setting up the ${DEMO}                                ##"
-echo "##                                                             ##"   
-echo "##                                                             ##"   
+echo "##                                                             ##"
+echo "##                                                             ##"
 echo "##     ####  ####   #   #      ### #   # ##### ##### #####     ##"
 echo "##     #   # #   # # # # #    #    #   #   #     #   #         ##"
 echo "##     ####  ####  #  #  #     ##  #   #   #     #   ###       ##"
 echo "##     #   # #     #     #       # #   #   #     #   #         ##"
 echo "##     ####  #     #     #    ###  ##### #####   #   #####     ##"
-echo "##                                                             ##"   
-echo "##                                                             ##"   
-echo "##  brought to you by,                                         ##"   
+echo "##                                                             ##"
+echo "##                                                             ##"
+echo "##  brought to you by,                                         ##"
 echo "##             ${AUTHORS}                  ##"
-echo "##                                                             ##"   
+echo "##                                                             ##"
 echo "##  ${PROJECT}      ##"
-echo "##                                                             ##"   
+echo "##                                                             ##"
 echo "#################################################################"
 echo
 
 
-# make some checks first before proceeding.	
+# make some checks first before proceeding.
 if [ -r $SRC_DIR/$EAP ] || [ -L $SRC_DIR/$EAP ]; then
 		echo Product sources are present...
 		echo
 else
-		echo Need to download $EAP package from the Customer Portal 
+		echo Need to download $EAP package from the Customer Portal
 		echo and place it in the $SRC_DIR directory to proceed...
 		echo
 		exit
 fi
 
-if [ -r $SRC_DIR/$EAP_PATCH ] || [ -L $SRC_DIR/$EAP_PATCH ]; then
-		echo Product patches are present...
-		echo
-else
-		echo Need to download $EAP_PATCH package from the Customer Portal 
-		echo and place it in the $SRC_DIR directory to proceed...
-		echo
-		exit
-fi
+#if [ -r $SRC_DIR/$EAP_PATCH ] || [ -L $SRC_DIR/$EAP_PATCH ]; then
+#		echo Product patches are present...
+#		echo
+#else
+#		echo Need to download $EAP_PATCH package from the Customer Portal
+#		echo and place it in the $SRC_DIR directory to proceed...
+#		echo
+#		exit
+#fi
 
 if [ -r $SRC_DIR/$BPMS ] || [ -L $SRC_DIR/$BPMS ]; then
 		echo Product sources are present...
 		echo
 else
-		echo Need to download $BPMS package from the Customer Portal 
+		echo Need to download $BPMS package from the Customer Portal
 		echo and place it in the $SRC_DIR directory to proceed...
 		echo
 		exit
@@ -89,22 +90,22 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
-echo
-echo "Applying JBoss EAP 6.4.4 patch now..."
-echo
-$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH"
+#echo
+#echo "Applying JBoss EAP 6.4.4 patch now..."
+#echo
+#$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH"
+#
+#if [ $? -ne 0 ]; then
+#	echo
+#	echo Error occurred during JBoss EAP patching!
+#	exit
+#fi
 
-if [ $? -ne 0 ]; then
-	echo
-	echo Error occurred during JBoss EAP patching!
-	exit
-fi
-
 echo
-echo "JBoss BPM Suite installer running now..."
+echo "Deploying JBoss BPM Suite now..."
 echo
-java -jar $SRC_DIR/$BPMS $SUPPORT_DIR/installation-bpms -variablefile $SUPPORT_DIR/installation-bpms.variables
-
+unzip -qo $SRC_DIR/$BPMS -d $TARGET
+#java -jar $SRC_DIR/$BPMS $SUPPORT_DIR/installation-bpms -variablefile $SUPPORT_DIR/installation-bpms.variables
 if [ $? -ne 0 ]; then
 	echo
 	echo Error occurred during JBoss BPM Suite installation!
@@ -112,9 +113,10 @@ if [ $? -ne 0 ]; then
 fi
 
 echo
-echo "  - enabling demo accounts role setup in application-roles.properties file..."
+echo "  - enabling demo accounts setup ..."
 echo
-cp $SUPPORT_DIR/application-roles.properties $SERVER_CONF
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u bpmsAdmin -p bpmsuite1! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u erics -p bpmsuite1! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent
 
 echo "  - setting up standalone.xml configuration adjustments..."
 echo
@@ -131,9 +133,8 @@ chmod u+x $JBOSS_HOME/bin/standalone.sh
 
 echo "You can now start the $PRODUCT with $SERVER_BIN/standalone.sh"
 echo
-echo "Login to http://localhost:8080/business-central   (u:erics / p:bpmsuite1!)"
+echo "Login to http://localhost:8080/business-central   (u:bpmsAdmin / p:bpmsuite1!)"
 echo
 
 echo "$PRODUCT $VERSION $DEMO Setup Complete."
 echo
-
